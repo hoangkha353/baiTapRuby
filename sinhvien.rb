6bc2ceee9b295de22d7d2f58f578beaf7af65a31
Hoàng Khá <hoangkha180301@gmail.com>
2022-09-16 21:24:32 +0700

    them bai tap 2

---
+++ sinhvien.rb
@@ -0,0 +1,46 @@
+class Sinhvien
+    attr_accessor :name, :id, :mLy, :mHoa, :mToan
+  
+    def initialize(name, id)
+      @name = name
+      @id = id
+    end
+  
+    def set_name(name)
+      @name = name
+    end
+  
+    def get_name
+      @name
+    end
+  
+    def set_id(id)
+      @id = id
+    end
+  
+    def get_id
+      @id
+    end
+  
+    def nhap_diem_toan(mToan)
+      @mToan = mToan
+    end
+  
+    def nhap_diem_ly(mLy)
+      @mLy = mLy
+    end
+  
+    def nhap_diem_hoa(mHoa)
+      @mHoa = mHoa
+    end
+  
+    def get_dtb
+      (@mToan + mLy + mHoa) / 3.0
+    end
+  
+    def display
+      puts "Tên sinh viên: #{@name}"
+      puts "Mã sinh viên: #{@id}"
+      puts "Điểm trung bình: #{get_dtb().round(2)}"
+    end
+end
